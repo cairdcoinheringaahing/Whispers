9218bddd852eaf836a18fc852d6d866b36a75899
cairdcoinheringaahing <valyrio.email@gmail.com>
2018-05-15 21:49:16 +0100

    Update whispers.py

--- whispers.py
+++ whispers.py
@@ -20,7 +20,7 @@ findall = lambda string, regex: list(filter(None, regex.match(string).groups()))
 normalise = lambda string: int(frombase(list(map(unicodedata.numeric, string)), 10))
 
 PRED    = B + 'ℂℕℙℝ' + U + 'ℤ¬⊤⊥'
-INFIX   = '=≠><≥≤+-±⋅×÷*%∆∩∪⊆⊂⊄⊅⊃⊇∖∈∉«»∤∣⊓⊔∘⊤⊥…⍟ⁱⁿ‖ᶠᵗ∓∕∠≮≯≰≱∧∨⋇⊼⊽∢⊿j≪≫'
+INFIX   = '=≠><≥≤+-±⋅×÷*%∆∩∪⊆⊈⊂⊄⊅⊃⊉⊇∖∈∉«»∤∣⊓⊔∘⊤⊥…⍟ⁱⁿ‖ᶠᵗ∓∕∠≮≯≰≱∧∨⋇⊼⊽∢⊿j≪≫'
 PREFIX  = "∑∏#√?'Γ∤℘ℑℜ∁≺≻∪⍎"
 POSTFIX = '!’#²³ᵀᴺ°ᴿ'
 OPEN    = '|(\[⌈⌊{"'
@@ -292,6 +292,8 @@ INFIX_ATOMS = {
 	'j':lambda a, b: complex(a, b),
 	'≪':lambda a, b: a << b,
 	'≫':lambda a, b: a >> b,
+	'⊈':lambda a, b: a.issubset(b) and a != b,
+	'⊉':lambda a, b: a.issuperset(b) and a != b,
 
 }
 
