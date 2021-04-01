c00cab4ba2f9254d6ad65a732a43ce3afe200d23
cairdcoinheringaahing <valyrio.email@gmail.com>
2021-04-01 15:37:25 +0100

    Accidentally deleted the latest changes with that bug fix

--- whispers v3.py
+++ whispers v3.py
@@ -34,9 +34,6 @@ import simplify
 
 sys.setrecursionlimit(1 << 30)
 
-sys.stdin = open('input')
-sys.argv.append('prog.ws')
-
 # Functions
 
 if not hasattr(math, 'gcd'):
@@ -85,7 +82,7 @@ CHARACTERISTIC = 0
 
 # ₀₁₂₃₄₅₆₇₈₉⁰¹²³⁴⁵⁶⁷⁸⁹
 
-INFIX   = '=≠><≥≤+-±⋅×÷*%∆∩∪⊆⊂⊄⊅⊃⊇∖∈∉«»∤∣⊓⊔∘⊤⊥…⍟ⁱⁿ‖ᶠᵗ∓∕∠≮≯≰≱∧∨⋇⊼⊽⊿j≪≫⊈⊉½→∥∦⟂⊾∡√CP?∔∸δMζΓγΒ⊞⋕⋚⋛⟌⧺⧻⨥⨧⨪⩱⩲'
+INFIX   = '=≠><≥≤+-±⋅×÷*%∆∩∪⊆⊂⊄⊅⊃⊇∖∈∉«»∤∣⊓⊔∘⊤⊥…⍟ⁱⁿ‖ᶠᵗˡ∓∕∠≮≯≰≱∧∨⋇⊼⊽⊿j≪≫⊈⊉½→∥∦⟂⊾∡√CP?∔∸δMζΓγΒ⊞⋕⋚⋛⟌⧺⧻⨥⨧⨪⩱⩲'
 INFIXES = ['∂Β']
 PREFIX  = "∑∏#√?'Γ∤℘ℑℜ∁≺≻∪⍎R…∛\-!∂∫IZ⊦⨊"
 POSTFIX = '!’#²³ᵀᴺ°ᴿ₁ᶜ?⊹'
@@ -2107,6 +2104,7 @@ INFIX_ATOMS = {
     'ⁿ': lambda a, b: a[b % len(a)],
     '‖': lambda a, b: (list(a) if hasattr(a, '__iter__') else [a]) + (list(b) if hasattr(a, '__iter__') else [b]),
     'ᶠ': lambda a, b: a[:b] if hasattr(a, '__iter__') else tobase(a, 10)[:b],
+    'ˡ': lambda a, b: a[len(a)-b:] if hasattr(a, '__iter__') else tobase(a, 10)[len(tobase(a, 10))-b:],
     'ᵗ': lambda a, b: a[b:] if hasattr(a, '__iter__') else tobase(a, 10)[b:],
     '∓': lambda a, b: [a-b, a+b],
     '∕': lambda a, b: int(a / b),
